18095fb9da177f47e525a008d3d622a3d58b7868
Alexander Vaganov <ar.vaganov@gmail.com>
2018-03-08 22:33:11 +0300

    rerender gitpages

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const data = [
         title: "Civilization VI: Rise and Fall review: A few turns closer to a Golden Age",
         titleColor: '#576433',
         image: "img/1.png",
-        description: "Every Civilization game since Civilization IV has followed the same trajectory: the initial release remixes and reinterprets some base systems from the previous game, but franchise veterans deem it anemic because it has fewer systems and features than its fully expanded predecessor. From there, new expansions gradually reintroduce the complexity that was lost in the move to a new game until many of those players conclude that it is the best game in the series yet.",
+        description: "Every Civilization game23 since Civilization IV has followed the same trajectory: the initial release remixes and reinterprets some base systems from the previous game, but franchise veterans deem it anemic because it has fewer systems and features than its fully expanded predecessor. From there, new expansions gradually reintroduce the complexity that was lost in the move to a new game until many of those players conclude that it is the best game in the series yet.",
         size: "m",
     },
     {
